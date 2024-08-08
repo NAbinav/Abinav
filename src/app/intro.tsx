9eb167adf14674986f6a86ce6a41e5bf8ec4903a
NAbinav <abinav0506@gmail.com>
2024-08-09 00:48:52 +0530

    skills

--- src/app/intro.tsx
+++ src/app/intro.tsx
@@ -1,23 +1,22 @@
 "use client";
 import { useEffect } from "react";
-import React, { MouseEvent } from 'react';
+import React, { MouseEvent } from "react";
 function Intro() {
-        useEffect(()=>{
-            const observer= new IntersectionObserver((entries)=>{
-                entries.forEach((entry)=>{
-                    if (entry.isIntersecting){
-                        entry.target.classList.add('show');
-                    }
-                    else{
-                        entry.target.classList.remove('show');
-                    }
-                })
-            })
-            const hiddenElement=document.querySelectorAll('.hidden');
-            hiddenElement.forEach((el)=>observer.observe(el));
-          });
   useEffect(() => {
-    const handleMouseMove = (e:any) => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("show");
+        } else {
+          entry.target.classList.remove("show");
+        }
+      });
+    });
+    const hiddenElement = document.querySelectorAll(".hidden");
+    hiddenElement.forEach((el) => observer.observe(el));
+  });
+  useEffect(() => {
+    const handleMouseMove = (e: any) => {
       const { currentTarget: target } = e;
       const rect = target.getBoundingClientRect();
       const x = e.clientX - rect.left;
@@ -25,12 +24,12 @@ function Intro() {
       target.style.setProperty("--mouse-x", `${x}px`);
       target.style.setProperty("--mouse-y", `${y}px`);
     };
-  
+
     const cards = document.querySelectorAll<HTMLElement>(".card");
     for (const card of cards) {
-      card.onmousemove = (e:any) => handleMouseMove(e);
+      card.onmousemove = (e: any) => handleMouseMove(e);
     }
-  
+
     // Cleanup function to remove event listeners
     return () => {
       for (const card of cards) {
@@ -40,53 +39,70 @@ function Intro() {
   }, []);
 
   return (
-    <main style={{height: '100  dvh',display:"flex", justifyContent:"center", alignContent:"center",alignItems:"center"}} className="hidden firstpage">
-      <div id="cards" >
-        <div className="card" onClick={()=>{
+    <main
+      style={{
+        height: "100  dvh",
+        display: "flex",
+        justifyContent: "center",
+        alignContent: "center",
+        alignItems: "center",
+      }}
+      className="hidden firstpage"
+    >
+      <div id="cards">
+        <div
+          className="card"
+          onClick={() => {
             window.scrollBy({
-                top: window.innerHeight,
-                left: 0,
-                behavior: "smooth",
-              });
-
-        }}>
+              top: window.innerHeight,
+              left: 0,
+              behavior: "smooth",
+            });
+          }}
+        >
           <div className="card-content">
             <p>HOME</p>
           </div>
         </div>
-        <div className="card" onClick={()=>{
+        <div
+          className="card"
+          onClick={() => {
             window.scrollBy({
-                top: 2*window.innerHeight,
-                left: 0,
-                behavior: "smooth",
-              });
-
-        }}>
+              top: 2 * window.innerHeight,
+              left: 0,
+              behavior: "smooth",
+            });
+          }}
+        >
           <div className="card-content">
             <p>ABOUT</p>
           </div>
         </div>
-        <div className="card" onClick={()=>{
+        <div
+          className="card"
+          onClick={() => {
             window.scrollBy({
-                top: 3.5*window.innerHeight,
-                left: 0,
-                behavior: "smooth",
-              });
-
-        }}>
-        <div className="card-content">
+              top: 3.25 * window.innerHeight,
+              left: 0,
+              behavior: "smooth",
+            });
+          }}
+        >
+          <div className="card-content">
             <p>SKILLS</p>
           </div>
         </div>
-        <div className="card" onClick={()=>{
+        <div
+          className="card"
+          onClick={() => {
             window.scrollBy({
-                top:4.5*window.innerHeight,
-                left: 0,
-                behavior: "smooth",
-              });
-
-        }}>
-        <div className="card-content">
+              top: 4.5 * window.innerHeight,
+              left: 0,
+              behavior: "smooth",
+            });
+          }}
+        >
+          <div className="card-content">
             <p>PROJECT</p>
           </div>
         </div>
@@ -95,4 +111,4 @@ function Intro() {
   );
 }
 
-export default Intro
+export default Intro;
--- src/app/skills.tsx
+++ src/app/skills.tsx
@@ -1,31 +1,43 @@
 "use client";
 import React from "react";
-const circleStyle = {
-  width: "275px",
-  height: "275px",
-  borderRadius: "50%",
-  border: "2px solid white",
-  margin: "10px",
-};
+import { Reveal } from "react-awesome-reveal";
+import { Name } from "./home";
+import { others } from "./home";
 
 const containerStyle = {};
 
 const Skills = () => {
+  let a = "275px";
+  const circleStyle = {
+    width: a,
+    height: a,
+    borderRadius: "50%",
+    border: "2px solid white",
+    margin: "10px",
+  };
   return (
     <main className="">
-      <div style={{ height: "100dvh" }} className="container">
-        <div style={circleStyle} className="circle">
-          <div>Devolopment </div>
-        </div>
-        <div style={circleStyle} className="circle">
-          <div>Programming Languages</div>
-        </div>
-        <div style={circleStyle} className="circle">
-          <div>Database</div>
-        </div>
-        <div style={circleStyle} className="circle">
-          <div>Graphic Designing</div>
-        </div>
+      <div style={{ height: "100dvh" }} className="container ">
+        <Reveal keyframes={Name} delay={300}>
+          <div style={circleStyle} className="circle hover:w-80 h-80">
+            <div>Devolopment </div>
+          </div>
+        </Reveal>
+        <Reveal keyframes={Name} delay={250}>
+          <div style={circleStyle} className="circle">
+            <div>Programming Languages </div>
+          </div>
+        </Reveal>
+        <Reveal keyframes={Name} delay={300}>
+          <div style={circleStyle} className="circle">
+            <div>Database </div>
+          </div>
+        </Reveal>
+        <Reveal keyframes={Name}>
+          <div style={circleStyle} className="circle">
+            <div>Designing </div>
+          </div>
+        </Reveal>
       </div>
     </main>
   );
