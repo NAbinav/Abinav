fe4ae58528aa03008e3a7d0f6f20580e2fa083a5
NAbinav <abinav0506@gmail.com>
2024-08-10 23:20:12 +0530

    Devolopment

--- src/app/skills.tsx
+++ src/app/skills.tsx
@@ -3,15 +3,37 @@ import React, { useState } from "react";
 import { Reveal } from "react-awesome-reveal";
 import { Name } from "./home";
 import { others } from "./home";
-import { FaReact, FaHtml5, FaJs, FaCss3 } from "react-icons/fa";
+import { FaReact, FaHtml5, FaJs, FaCss3, FaNodeJs } from "react-icons/fa";
+import { SiTailwindcss } from "react-icons/si";
+
 import Circle from "../../components/Circles";
 
 const containerStyle = {};
 const list1 = [
-  <FaHtml5 key="html5" />,
-  <FaJs key="js" />,
-  <FaReact key="react" />,
-  <FaCss3 key="css3" />,
+  <FaHtml5
+    key="html5"
+    className="h-12 w-12  p-2 rounded-full border-2 text-[#FF5225] shadow-[0_0px_20px]"
+  />,
+  <FaJs
+    key="js"
+    className=" h-12 w-12  p-2 rounded-full border-2 text-[#F7E025] shadow-[0_0px_20px]"
+  />,
+  <SiTailwindcss
+    key="Tailwind"
+    className=" h-12 w-12  p-2 rounded-full border-2 text-[#0BB5D5] shadow-[0_0px_20px]"
+  />,
+  <FaReact
+    key="react"
+    className=" h-12 w-12  p-2 rounded-full border-2 text-[#08D9FF] shadow-[0_0px_20px]"
+  />,
+  <FaCss3
+    key="css3"
+    className=" h-12 w-12  p-2 rounded-full border-2 text-[#2629e4] shadow-[0_0px_20px]"
+  />,
+  <FaNodeJs
+    key="NodeJs"
+    className=" h-12 w-12  p-2 rounded-full border-2 text-[#84BF08] shadow-[0_0px_20px]"
+  />,
 ];
 
 const Skills = () => {
@@ -19,25 +41,25 @@ const Skills = () => {
     <main className="">
       <div style={{ height: "100dvh" }} className="container ">
         <Reveal keyframes={Name} delay={300}>
-          <div className="circle  group hover:shadow-[0_0px_60px] hover:shadow-rose-500">
+          <div className="circle  group hover:shadow-[0_0px_40px] hover:shadow-rose-500">
             <div className="group-hover:hidden">Devolopment </div>
-            <div className="hidden group-hover:block -translate-x-9 -translate-y-9">
+            <div className="hidden group-hover:block -translate-x-10 -translate-y-9">
               <Circle list={list1} />
             </div>
           </div>
         </Reveal>
         <Reveal keyframes={Name} delay={250}>
-          <div className="circle group hover:shadow-[0_0px_60px] hover:shadow-emerald-600 ">
+          <div className="circle group hover:shadow-[0_0px_40px] hover:shadow-emerald-600 ">
             <div className="group-hover:hidden">Programming Languages </div>
           </div>
         </Reveal>
         <Reveal keyframes={Name} delay={300}>
-          <div className="circle group hover:shadow-[0_0px_60px] hover:shadow-sky-600">
+          <div className="circle group hover:shadow-[0_0px_40px] hover:shadow-sky-600">
             <div className="group-hover:hidden">Database </div>
           </div>
         </Reveal>
         <Reveal keyframes={Name}>
-          <div className="circle group hover:shadow-[0_0px_60px] hover:shadow-fuchsia-600">
+          <div className="circle group hover:shadow-[0_0px_40px] hover:shadow-fuchsia-600">
             <div className="group-hover:hidden">Designing </div>
           </div>
         </Reveal>
