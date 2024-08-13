41a40685f1a023a510e8991e4519ec5e3bc167eb
NAbinav <abinav0506@gmail.com>
2024-08-13 20:17:19 +0530

    responsive

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <meta
         name="viewport"
-        content="width=device-width, initial-scale=1.00"
+        content="width=device-width, user-scalable=no"
       ></meta>
       <body className={inter.className}>
         <main className="">
